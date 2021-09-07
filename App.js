498d53fa206f75c4dade000d0be7c9a89461c81f
Dave Genest <drwgenest@gmail.com>
2021-09-07 16:29:05 -0700

    Adding weather icons

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading'
+import { useFonts } from 'expo-font'
 import { WeatherAppProvider } from './src/store/WeatherAppContext'
 import Home from './src/components/Home'
 
 export default function App() {
-  return (
+  let [fontsLoaded] = useFonts({
+    'WeatherIcons': require('./assets/fonts/WeatherIcons.ttf'),
+  })
+  return fontsLoaded ? (
     <WeatherAppProvider>
       <Home />
     </WeatherAppProvider>
-  );
+  ) : <AppLoading />
 }
