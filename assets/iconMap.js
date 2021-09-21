2608bdee90198fcd92442a4cf4db19fbf0cb1f27
Dave Genest <drwgenest@gmail.com>
2021-09-20 21:12:12 -0700

    Basic forecast summary

---
+++ assets/iconMap.js
@@ -0,0 +1,173 @@
+// from https://api.weather.gov/icons
+export default {
+  skc: {
+    day: 'wi_day_sunny',
+    night: 'wi_lunar_eclipse',
+    description: 'Fair/clear'
+  },
+  few: {
+    day: 'wi_day_cloudy',
+    night: 'wi_night_alt_cloudy',
+    description: 'A few clouds'
+  },
+  sct: {
+    day: 'wi_day_cloudy',
+    night: 'wi_night_alt_partly_cloudy',
+    description: 'Partly cloudy'
+  },
+  bkn: {
+    day: 'wi_day_cloudy_high',
+    night: 'wi_night_alt_cloudy_high',
+    description: 'Mostly cloudy'
+  },
+  ovc: {
+    day: 'wi_day_sunny_overcast',
+    night: 'wi_night_alt_partly_cloudy',
+    description: 'Overcast'
+  },
+  wind_skc: {
+    day: 'wi_day_wind',
+    night: 'wi_night_alt_cloudy_gusts',
+    description: 'Fair/clear and windy'
+  },
+  wind_few: {
+    day: 'wi_day_cloudy_gusts',
+    night: 'wi_night_alt_cloudy_gusts',
+    description: 'A few clouds and windy'
+  },
+  wind_sct: {
+    day: 'wi_day_cloudy_gusts',
+    night: 'wi_night_alt_cloudy_gusts',
+    description: 'Partly cloudy and windy'
+  },
+  wind_bkn: {
+    day: 'wi_cloudy_gusts',
+    night: 'wi_cloudy_gusts',
+    description: 'Mostly cloudy and windy'
+  },
+  wind_ovc: {
+    day: 'wi_cloudy_windy',
+    night: 'wi_cloudy_windy',
+    description: 'Overcast and windy'
+  },
+  snow: {
+    day: 'wi_snow',
+    night: 'wi_snow',
+    description: 'Snow'
+  },
+  rain_snow: {
+    day: 'wi_rain_mix',
+    night: 'wi_rain_mix',
+    description: 'Rain/snow'
+  },
+  rain_sleet: {
+    day: 'wi_rain_mix',
+    night: 'wi_rain_mix',
+    description: 'Rain/sleet'
+  },
+  snow_sleet: {
+    day: 'wi_day_sleet',
+    night: 'wi_night_alt_sleet',
+    description: 'Snow/sleet'
+  },
+  fzra: {
+    day: 'wi_day_sleet',
+    night: 'wi_night_alt_sleet',
+    description: 'Freezing rain'
+  },
+  rain_fzra: {
+    day: 'wi_hail',
+    night: 'wi_hail',
+    description: 'Rain/freezing rain'
+  },
+  snow_fzra: {
+    day: 'wi_hail',
+    night: 'wi_hail',
+    description: 'Freezing rain/snow'
+  },
+  sleet: {
+    day: 'wi_hail',
+    night: 'wi_hail',
+    description: 'Sleet'
+  },
+  rain: {
+    day: 'wi_day_rain',
+    night: 'wi_night_alt_rain',
+    description: 'Rain'
+  },
+  rain_showers: {
+    day: 'wi_day_sprinkle',
+    night: 'wi_night_alt_sprinkle',
+    description: 'Rain showers (high cloud cover)'
+  },
+  rain_showers_hi: {
+    day: 'wi_day_sprinkle',
+    night: 'wi_night_alt_sprinkle',
+    description: 'Rain showers (low cloud cover)'
+  },
+  tsra: {
+    day: 'wi_day_thunderstorm',
+    night: 'wi_night_alt_thunderstorm',
+    description: 'Thunderstorm (high cloud cover)'
+  },
+  tsra_sct: {
+    day: 'wi_day_thunderstorm',
+    night: 'wi_night_alt_thunderstorm',
+    description: 'Thunderstorm (medium cloud cover)'
+  },
+  tsra_hi: {
+    day: 'wi_day_thunderstorm',
+    night: 'wi_night_alt_thunderstorm',
+    description: 'Thunderstorm (low cloud cover)'
+  },
+  tornado: {
+    day: 'wi_tornado',
+    night: 'wi_tornado',
+    description: 'Tornado'
+  },
+  hurricane: {
+    day: 'wi_hurricane',
+    night: 'wi_hurricane',
+    description: 'Hurricane conditions'
+  },
+  tropical_storm: {
+    day: 'wi_hurricane',
+    night: 'wi_hurricane',
+    description: 'Tropical storm conditions'
+  },
+  dust: {
+    day: 'wi_sandstorm',
+    night: 'wi_sandstorm',
+    description: 'Dust'
+  },
+  smoke: {
+    day: 'wi_smoke',
+    night: 'wi_smoke',
+    description: 'Smoke'
+  },
+  haze: {
+    day: 'wi_day_haze',
+    night: 'wi_day_haze',
+    description: 'Haze'
+  },
+  hot: {
+    day: 'wi_hot',
+    night: 'wi_hot',
+    description: 'Hot'
+  },
+  cold: {
+    day: 'wi_snowflake_cold',
+    night: 'wi_snowflake_cold',
+    description: 'Cold'
+  },
+  blizzard: {
+    day: 'wi_day_snow_thunderstorm',
+    night: 'wi_night_alt_snow_thunderstorm',
+    description: 'Blizzard'
+  },
+  fog: {
+    day: 'wi_day_fog',
+    night: 'wi_night_fog',
+    description: 'Fog/mist'
+  }
+}
